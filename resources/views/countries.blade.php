b1295ff4401cc4fc2710f300237d5a6642f63593
noppadolr <riyasarn.n@gmail.com>
2024-01-30 20:02:09 +0700

    init

---
+++ resources/views/countries.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>World Countries</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link href=" https://cdn.jsdelivr.net/npm/sweetalert2@11.10.4/dist/sweetalert2.min.css " rel="stylesheet">
+    @stack('styles')
+</head>
+<body>
+
+    <div class="container">
+        <div class="row" style="margin-top: 45px">
+            <div class="col-md-6 offset-md-3">
+                <h4>World Countries</h4>
+                <button type="button" class="btn btn-outline-primary btn-sm">Add New Country</button>
+                @livewire('countries')
+
+            </div>
+        </div>
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" ></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" ></script>
+    <script src=" https://cdn.jsdelivr.net/npm/sweetalert2@11.10.4/dist/sweetalert2.all.min.js "></script>
+
+@stack('scripts')
+
+
+</body>
+</html>
---
+++ resources/views/livewire/countries.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <table class="table  table-hover table-responsive">
+        <thead class="thead-inverse">
+        <tr>
+            <th>Continent</th>
+            <th>Country</th>
+            <th>Capital City</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td>Asia</td>
+            <td>China</td>
+            <td>Beijing</td>
+        </tr>
+        </tbody>
+
+    </table>
+</div>
